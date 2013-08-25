e7fec84deea9f849ca8eeb99d3d9ca916a9f17ed
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-08-24 20:08:53 -0700

    Full coverage

--- spec/lib/digitalfilmtree/vfx/edl_ml_renamer_spec.rb
+++ spec/lib/digitalfilmtree/vfx/edl_ml_renamer_spec.rb
@@ -2,18 +2,25 @@ require 'spec_helper'
 require 'digitalfilmtree/vfx/edl_ml_renamer'
 
 describe Digitalfilmtree::VFX::EDLMLRenamer do
-  let(:renamer) { subject }
-
-  it "requires a target folder path" do
-    renamer.folder = "foo/bar"
-    renamer.folder.should eq "foo/bar"
-  end
-
   describe "#execute" do
     context "missing required files" do
       specify do
-        expect{renamer.execute}.to raise_error "Not Ready"
+        expect{subject.execute}.to raise_error "Not Ready"
       end
+    end
+
+    context "required files exist" do
+      let(:target) { fixture('vfx/edl_ml_renamer') }
+      before do
+        subject.folder = target.path
+        @originals = subject.movs.map{|i| Pathname.new(i)}
+      end
+      it "renames .mov files in the folder" do
+        @originals.each {|i| i.should exist }
+        subject.execute
+        @originals.each {|i| i.should_not exist }
+      end
+      after { target.clean.reset }
     end
   end
 end
