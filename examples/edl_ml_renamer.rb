b7655e3687c00de1cb4bc2bc0f6abea9eef5f9f6
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-08-28 00:43:53 -0700

    Update edl_ml_renamer.rb
    Improved example script

--- examples/edl_ml_renamer.rb
+++ examples/edl_ml_renamer.rb
@@ -2,19 +2,24 @@
 # This script will run the EDL ML Renamer from wherever it is executed
 
 require 'rubygems'
-
-PATH = File.expand_path(File.dirname(__FILE__)).
-  gsub(File::SEPARATOR, File::ALT_SEPARATOR || File::SEPARATOR)
-
 require 'digitalfilmtree/vfx/edl_ml_renamer'
-r = Digitalfilmtree::VFX::EDLMLRenamer.new
-r.folder = PATH
 
-if r.ready?
-  STDOUT.puts "Do you want me to rename?"
-else
-  STDOUT.puts "Not ready. Are you missing .txt, .edl, and/or .mov files?"
+include Digitalfilmtree
+
+r = VFX::EDLMLRenamer.new
+r.folder = Util.winpath File.expand_path File.dirname __FILE__
+
+begin
+  Util::Mediainfo.autoconfigure
+  if r.ready?
+    r.execute
+  else
+    puts "Not ready. Are you missing .txt, .edl, and/or .mov files?"
+    puts "I expect these files to exist in the same directory"
+  end
+rescue Exception => ex
+  puts "Failed. Reason: #{ex.message}"
+ensure
+  puts "Press enter to quit"
+  gets
 end
-STDIN.gets
-
-r.execute
