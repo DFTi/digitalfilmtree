d4c94aa620ad0911d8525c9756591fc6e9e34ef5
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-08-28 06:14:52 -0700

    Fold Platform into Util#platform, refactor Util

--- lib/digitalfilmtree/util.rb
+++ lib/digitalfilmtree/util.rb
@@ -1,21 +1,47 @@
 module Digitalfilmtree
   module Util
-    def self.winpath path
-      path.gsub(File::SEPARATOR, File::ALT_SEPARATOR || File::SEPARATOR)
+    FILE_SEP = File::ALT_SEPARATOR || File::SEPARATOR
+
+    ##
+    # Pass a file path through this to fix separators
+    def self.safe_path path
+      path.gsub(File::SEPARATOR, FILE_SEP)
     end
 
-    def self.platform
-      require 'digitalfilmtree/platform'
-      Digitalfilmtree.platform
-    end
-
+    ##
+    # Grab a vendored binary
     def self.vendored_bin name, platform, filename
-      File.expand_path(
-        File.join(
-          File.dirname(__FILE__), '..', '..',
-          'vendor', 'util', name.to_s, 
-          platform.to_s, filename
-        ))
+      Util.safe_path(File.expand_path(
+        File.join(File.dirname(__FILE__), 
+                  '..', '..','vendor', 'util', 
+                  name.to_s, platform.to_s, filename)))
+    end
+
+    ##
+    # Introspect about the platform
+    def self.platform
+      require 'rbconfig'
+      @platform ||= Class.new do
+        def initialize
+          @os = RbConfig::CONFIG['host_os']
+        end
+
+        def windows?
+          @os =~ /mswin|mingw|cygwin/
+        end
+
+        def mac?
+          @os =~ /darwin/
+        end
+
+        def linux?
+          @os =~ /linux/
+        end
+
+        def to_s
+          @os
+        end
+      end.new
     end
   end
 end
